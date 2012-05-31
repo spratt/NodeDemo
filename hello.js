873955ddd1efd34aef769288d2ff8f468ff670d7
Simon Pratt <me@SimonDavidPratt.com>
2012-05-30 19:19:39 -0500

    now a real node.js module

--- hello.js
+++ hello.js
@@ -1,5 +1,23 @@
+/******************************************************************************
+* Requirements                                                                *
+******************************************************************************/
 var http = require('http');
-http.createServer(function (request, response) {
-  response.writeHead(200, {'Content-Type': 'text/plain'});
-  response.end('Hello World\n');
-}).listen(14974);
+var url  = require('url');
+
+/******************************************************************************
+* Variables                                                                   *
+******************************************************************************/
+var __PORT__ = 14974;
+
+/******************************************************************************
+* Server function                                                             *
+******************************************************************************/
+function start() {
+    function onRequest(request, response) {
+	response.writeHead(200, {'Content-Type': 'text/plain'});
+	response.end('Hello World\n');
+    };
+    http.createServer(onRequest).listen(__PORT__);
+};
+
+exports.start = start;
---
+++ index.js
@@ -0,0 +1,3 @@
+// start the hello server
+var helloServer = require("./hello");
+helloServer.start();
