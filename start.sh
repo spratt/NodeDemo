b54aa251a2ad4ef5cb7c53b0ac983db7c4967e4e
Simon Pratt <me@SimonDavidPratt.com>
2012-05-30 19:51:54 -0500

    Nice enough start file

--- start.sh
+++ start.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
 NODE=/home/sdp/bin/node
-APP=/home/sdp/webapps/node_demo/hello.js
-$NODE $APP
+APP=/home/sdp/webapps/node_demo/index.js
+
+SERVER_FILE=server.pid
+
+# stop if already running
+if [[ -f $SERVER_FILE ]]; then
+    kill -9 `cat server.pid` & rm server.pid
+fi
+
+# start
+$NODE $APP &
+echo $! > $SERVER_FILE # write 
+
